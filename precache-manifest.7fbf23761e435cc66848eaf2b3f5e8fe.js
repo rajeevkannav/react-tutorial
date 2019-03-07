69d0c2eaed31622448562d3621aa54ba7db50bc2
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-03-07 19:46:20 +0530

    Updates

---
+++ precache-manifest.7fbf23761e435cc66848eaf2b3f5e8fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tutorial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9b2d15347dbf2dd6be24",
+    "url": "/react-tutorial/static/js/runtime~main.ebc3c14e.js"
+  },
+  {
+    "revision": "93d59da86a544c5f011d",
+    "url": "/react-tutorial/static/js/main.7d14e214.chunk.js"
+  },
+  {
+    "revision": "f6422e6e88cfb1ba2ef6",
+    "url": "/react-tutorial/static/js/2.372fd93a.chunk.js"
+  },
+  {
+    "revision": "93d59da86a544c5f011d",
+    "url": "/react-tutorial/static/css/main.7770a2c6.chunk.css"
+  },
+  {
+    "revision": "e341bda84edc5d6c171e258181581efc",
+    "url": "/react-tutorial/index.html"
+  }
+];
